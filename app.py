a39f9a52442ca52ee149dad24d6e3902b9fcbfb6
karkiking105-droid <karki.king105@gmail.com>
2025-09-29 12:03:24 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+st.set_page_config(page_title="AiSathi 🇳🇵", layout="centered")
+st.title("🤖 AiSathi")
+st.caption("Made in Nepal, Loved Worldwide 🌍")
+
+st.write("Namaste Gopal! Your app is now live 🎉")
